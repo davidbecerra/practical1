8fd50e6343095e130e7bb3d4e5bb31eb25e5cbe2
WENTAO_XU <wentaoxu@college.harvard.edu>
2015-02-13 10:02:44 -0500

    Missing 20000

--- train.py
+++ train.py
@@ -20,16 +20,17 @@ def read_data(train_csv, N):
 	for row in train_csv:
 		# Grab the smile data
 		smile = row[0]
-
+		m = Chem.MolFromSmiles(smile)
 		#Morgan Fingerprint --> optimal: 1, 1024 bits 
-		feats = np.array([float(x) for x in row[1:1025]])
+		feats = np.array(AllChem.GetMorganFingerprintAsBitVect(m, 1, nBits=1024, useFeatures=True))
 		all_feats.append(feats)
 
 		#Grab the gap data 
-		gap = float(row[1025])
+		gap = float(row[257])
 		gaps.append(gap)
-		
 		N -= 1
+		if N%1000 == 0:
+			print N
 		if N == 0: break
 
 	return (gaps, all_feats)
@@ -41,43 +42,42 @@ def rmse(train_csv, w, gaps, features):
 	return np.sqrt((np.sum(np.square(Y - Y_hat))) / N)
 	
 def cross_validation(all_gaps, all_features, N):
-	lam = 1
-	min_lam_err = float("inf")
-	while True:
-		print "start"
-		lam_err = 0
-		for i in xrange(0,5):
-			test_gaps = []
-			test_features = []
-			train_gaps = []
-			train_features = []
+  lam = .5
+  min_lam_err = float("inf")
+  while True:
+	print "start"
+	lam_err = 0
+	for i in xrange(0,5):
+	  test_gaps = []
+	  test_features = []
+	  train_gaps = []
+	  train_features = []
 
-			for j in xrange(0,N):
-				if j % 5 != i:
-					train_gaps.append(all_gaps[j])
-					train_features.append(all_features[j])
-				else:
-					test_gaps.append(all_gaps[j])
-					test_features.append(all_features[j])
-
-			Y = np.vstack(np.array(train_gaps))
-			X = (np.vstack((np.ones(4*N/5), (np.vstack(tuple(train_features))).T))).T
-			w = np.linalg.solve(np.dot(X.T, X) + lam * np.identity(1025), np.dot(X.T, Y))
-
-			test_Y = np.vstack(np.array(test_gaps)).T
-			test_X = (np.vstack((np.ones(N/5), (np.vstack(tuple(test_features))).T))).T
-			Y_hat = np.dot(test_X, w).T
-			lam_err += np.sum(np.square(Y - Y_hat))
-		
-		if lam_err/N < min_lam_err:
-			min_lam_err = lam_err/N
-			lam += 150
-			print lam
-			print min_lam_err
+	  for j in xrange(0,N):
+		if j % 5 != i:
+		  train_gaps.append(all_gaps[j])
+		  train_features.append(all_features[j])
 		else:
-			break
+		  test_gaps.append(all_gaps[j])
+		  test_features.append(all_features[j])
 
-	return lam
+	  Y = np.vstack(np.array(train_gaps))
+	  X = (np.vstack((np.ones(4*N/5), (np.vstack(tuple(train_features))).T))).T
+	  w = np.linalg.solve(np.dot(X.T, X) + lam * np.identity(1025), np.dot(X.T, Y))
+
+	  test_Y = np.vstack(np.array(test_gaps)).T
+	  test_X = (np.vstack((np.ones(N/5), (np.vstack(tuple(test_features))).T))).T
+	  Y_hat = np.dot(test_X, w).T
+	  lam_err += np.sum(np.square(test_Y - Y_hat))
+	
+	if lam_err/N < min_lam_err:
+	  min_lam_err = lam_err/N
+	  lam -= .07
+	  print min_lam_err
+	else:
+	  break
+
+  return lam
 
 def ridge_plus_validation(N):
 	train_filename = 'train.csv.gz'
@@ -98,37 +98,42 @@ def ridge_plus_validation(N):
 		Y = np.vstack(np.array(gaps))
 		X = (np.vstack((np.ones(N), (np.vstack(tuple(features))).T))).T
 		w = np.linalg.solve(np.dot(X.T, X) + lam * np.identity(1025), np.dot(X.T, Y))
-		return (w, lam)
+		return w
 
-if __name__ == "__main__":
-	print ridge_plus_validation(100000)
-	#read_data(10000)
-
-def predict(N, w, lam):
+def predict(N, w):
 	test_filename = 'final_features.csv.gz'
 	all_feats = []
-	w = 0.01
 	index = 1
 	# Load the test file
 	with gzip.open(test_filename, 'r') as test_fh:
 		# Parse it as a CSV file.
+
 		test_csv = csv.reader(test_fh, delimiter=',', quotechar='"')
+		#for x in range(0, 800000):
+			#next(test_csv, None)
 		with open('results.csv', 'w') as results_file:
 		# Grab the data.
+			results_model = csv.writer(results_file, delimiter=',', quotechar='"')
+			results_model.writerow(["Id"] + ["Prediction"])
+			next(test_csv, None)
 			for row in test_csv:
 				#Grab Features 
 				features = np.array([float(x) for x in row[1:1025]])
-				X = (np.vstack((np.ones(N), (np.vstack(tuple(features))).T))).T
-				Y_hat = np.dot(X, w).T
-				
+				#X = np.vstack(((1), (tuple(features)))).T
+				#X = (np.vstack((np.ones(1), (np.vstack(tuple(features))).T))).T
+				temp = np.vstack(tuple(features))
+				X = (np.vstack(((1), temp))).T
+				Y_hat = np.dot(X, w)[0][0]
 				results_model = csv.writer(results_file, delimiter=',', quotechar='"')
 				results_model.writerow([index] + [Y_hat])
-				index+1
+				index+=1
 				N -= 1
+				if N % 10000 == 0:
+					print N
 				if N == 0: break
 
 		return 0
 
 if __name__ == "__main__":
-	w, lam = ridge_plus_validation(100000)
-	predict(800000, w, lam)
+	w = ridge_plus_validation(50000)
+	predict(900000, w)
